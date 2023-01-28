7d2333e8ee6657e9619b081316814e065cbbc374
ZdravkoBarukchiev <112626604+ZdravkoBarukchiev@users.noreply.github.com>
2023-01-28 11:24:39 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import { About } from "./components/About/About";
 import { ClientSection } from "./components/ClientSection/ClientSection";
 import { Contact } from "./components/Contact/Contact";
 import { Copyright } from "./components/Copyright/Copyright";
 import { CycleSection } from "./components/CycleSection/CycleSection";
 import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
 import { News } from "./components/News/News";
 
 function App() {
   return (
     <>
-      <Header />
-      <CycleSection />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cycles' element={<CycleSection />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
       <About />
       <ClientSection />
       <News />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import { Navigation } from "./Navigation/Navigation"
 import { BanerSection } from "./BanerSection/BanerSection"
-export const Header =()=>{
+export const Home =()=>{
     return(
         <div className="header_section header_bg">
         <Navigation />
